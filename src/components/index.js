5b13f9e9fd84d7c9db46aeb09f36d7a00655f131
Samuel <samuel05.hb@gmail.com>
2026-08-14 16:51:38 -0400

    refactor: remove dead Feedbacks component

--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,6 @@ import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
 import Works from './Works';
-import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 
 export {
@@ -18,7 +17,6 @@ export {
   Tech,
   Experience,
   Works,
-  Feedbacks,
   Contact,
   EarthCanvas, 
   BallCanvas, 
